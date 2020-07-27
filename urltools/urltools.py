37803828216e099a5f841ae8a7525ac52dc2f0ad
itzik-h <itzikhoffnung@gmail.com>
2020-07-27 10:42:25 +0300

    Update to match version 0.3.2 of original urltools

--- urltools/urltools.py
+++ urltools/urltools.py
@@ -1,47 +1,41 @@
-"""
-Copyright (c) 2013 Roderick Baier
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-"""
-
 import os
-import re
-import urllib
+import codecs
 from collections import namedtuple
 from posixpath import normpath
 
+try:
+    from urllib.request import urlopen
+    from urllib.parse import quote
+    unicode = str
+except:
+    from urllib import urlopen
+    from urllib import quote
 
-__all__ = ["ParseResult", "SplitResult", "parse", "extract", "split",
-           "split_netloc", "split_host", "assemble", "encode", "normalize",
-           "normalize_host", "normalize_path", "normalize_query",
-           "normalize_fragment", "unquote"]
+
+__version__ = '0.3.2'
+
+__all__ = ['URL', 'SplitResult', 'parse', 'extract', 'construct', 'normalize',
+           'compare', 'normalize_host', 'normalize_path', 'normalize_query',
+           'normalize_fragment', 'encode', 'unquote', 'split', 'split_netloc',
+           'split_host']
 
 
-PSL_URL = 'http://mxr.mozilla.org/mozilla-central/source/netwerk/dns/effective_tld_names.dat?raw=1'
+PSL_URL = 'https://publicsuffix.org/list/effective_tld_names.dat'
+
 
 def _get_public_suffix_list():
-    """Get the public suffix list.
+    """Return a set containing all Public Suffixes.
+
+    If the env variable PUBLIC_SUFFIX_LIST does not point to a local copy of the
+    public suffix list it is downloaded into memory each time urltools is
+    imported.
     """
     local_psl = os.environ.get('PUBLIC_SUFFIX_LIST')
     if local_psl:
-        psl_raw = open(local_psl).readlines()
+        with codecs.open(local_psl, 'r', 'utf-8') as f:
+            psl_raw = f.readlines()
     else:
-        psl_raw = urllib.urlopen(PSL_URL).readlines()
+        psl_raw = unicode(urlopen(PSL_URL).read(), 'utf-8').split('\n')
     psl = set()
     for line in psl_raw:
         item = line.strip()
@@ -50,6 +44,7 @@ def _get_public_suffix_list():
     return psl
 
 PSL = _get_public_suffix_list()
+assert len(PSL) > 0, 'Public Suffix List is empty!'
 
 
 SCHEMES = ['http', 'https', 'ftp', 'sftp', 'file', 'gopher', 'imap', 'mms',
@@ -63,103 +58,141 @@ DEFAULT_PORT = {
     'ws': '80',
     'wss': '443',
     'ftp': '21',
-    'sftp': '22'
+    'sftp': '22',
+    'ldap': '389'
 }
-UNQUOTE_EXCEPTIONS = {
+QUOTE_EXCEPTIONS = {
     'path': ' /?+#',
     'query': ' &=+#',
     'fragment': ' +#'
 }
 
-_hextochr = {'%02x' % i: chr(i) for i in range(256)}
-_hextochr.update({'%02X' % i: chr(i) for i in range(256)})
-_idna_encode = lambda x: x.decode('utf-8').encode('idna')
-_idna_decode = lambda x: x.decode('idna').encode('utf-8')
 
 SplitResult = namedtuple('SplitResult', ['scheme', 'netloc', 'path', 'query',
                                          'fragment'])
-ParseResult = namedtuple('ParseResult', ['scheme', 'username', 'password',
-                                         'subdomain', 'domain', 'tld', 'port',
-                                         'path', 'query', 'fragment'])
+URL = namedtuple('URL', ['scheme', 'username', 'password', 'subdomain',
+                         'domain', 'tld', 'port', 'path', 'query', 'fragment',
+                         'url'])
 
 
 def normalize(url):
-    """Normalize a URL
+    """Normalize a URL.
+
+    >>> normalize('hTtp://ExAMPLe.COM:80')
+    'http://example.com/'
     """
+    url = url.strip()
     if url == '':
         return ''
-    parts = split(url.strip())
+    parts = split(url)
     if parts.scheme:
         netloc = parts.netloc
         if parts.scheme in SCHEMES:
             path = normalize_path(parts.path)
         else:
             path = parts.path
+    # url is relative, netloc (if present) is part of path
     else:
         netloc = parts.path
         path = ''
         if '/' in netloc:
-            tmp = netloc.split('/', 1)
-            netloc = tmp[0]
-            path = normalize_path('/' + tmp[1])
+            netloc, path_raw = netloc.split('/', 1)
+            path = normalize_path('/' + path_raw)
     username, password, host, port = split_netloc(netloc)
     host = normalize_host(host)
-    port = normalize_port(parts.scheme, port)
+    port = _normalize_port(parts.scheme, port)
     query = normalize_query(parts.query)
     fragment = normalize_fragment(parts.fragment)
-    result = ParseResult(parts.scheme, username, password, None, host, None,
-                         port, path, query, fragment)
-    return assemble(result)
+    return construct(URL(parts.scheme, username, password, None, host, None,
+                         port, path, query, fragment, None))
+
+
+def compare(url1, url2):
+    """Check if url1 and url2 are the same after normalizing both.
+
+    >>> compare("http://examPLe.com:80/abc?x=&b=1", "http://eXAmple.com/abc?b=1")
+    True
+    """
+    return normalize(url1) == normalize(url2)
+
+
+def _idna_encode(x):
+    return x.encode('idna').decode('utf-8')
+
+
+def _encode_query(query):
+    """Quote all values of a query string."""
+    if query == '':
+        return query
+    query_args = []
+    for query_kv in query.split('&'):
+        k, v = query_kv.split('=')
+        query_args.append(k + "=" + quote(v.encode('utf-8')))
+    return '&'.join(query_args)
 
 
 def encode(url):
-    """Encode URL
-    """
+    """Encode URL."""
     parts = extract(url)
-    encoded = ParseResult(*(_idna_encode(p) for p in parts))
-    return assemble(encoded)
+    return construct(URL(parts.scheme,
+                         parts.username,
+                         parts.password,
+                         _idna_encode(parts.subdomain),
+                         _idna_encode(parts.domain),
+                         _idna_encode(parts.tld),
+                         parts.port,
+                         quote(parts.path.encode('utf-8')),
+                         _encode_query(parts.query),
+                         quote(parts.fragment.encode('utf-8')),
+                         None))
 
 
-def assemble(parts):
-    """Assemble a ParseResult to a new URL
-    """
-    nurl = ''
+def construct(parts):
+    """Construct a new URL from parts."""
+    url = ''
     if parts.scheme:
         if parts.scheme in SCHEMES:
-            nurl += parts.scheme + '://'
+            url += parts.scheme + '://'
         else:
-            nurl += parts.scheme + ':'
+            url += parts.scheme + ':'
     if parts.username and parts.password:
-        nurl += parts.username + ':' + parts.password + '@'
+        url += parts.username + ':' + parts.password + '@'
     elif parts.username:
-        nurl += parts.username + '@'
+        url += parts.username + '@'
     if parts.subdomain:
-        nurl += parts.subdomain + '.'
-    nurl += parts.domain
+        url += parts.subdomain + '.'
+    url += parts.domain
     if parts.tld:
-        nurl += '.' + parts.tld
+        url += '.' + parts.tld
     if parts.port:
-        nurl += ':' + parts.port
+        url += ':' + parts.port
     if parts.path:
-        nurl += parts.path
+        url += parts.path
     if parts.query:
-        nurl += '?' + parts.query
+        url += '?' + parts.query
     if parts.fragment:
-        nurl += '#' + parts.fragment
-    return nurl
+        url += '#' + parts.fragment
+    return url
+
+
+def _idna_decode(x):
+    return codecs.decode(x.encode('utf-8'), 'idna')
 
 
 def normalize_host(host):
-    """Normalize host (decode IDNA)
-    """
+    """Normalize host (decode IDNA)."""
     if 'xn--' not in host:
         return host
-    parts = host.split('.')
-    return '.'.join([_idna_decode(p) for p in parts])
+    return '.'.join([_idna_decode(p) for p in host.split('.')])
 
 
-def normalize_port(scheme, port):
-    """Check if the port is default port
+def _normalize_port(scheme, port):
+    """Return port if it is not default port, else None.
+
+    >>> _normalize_port('http', '80')
+
+    >>> _normalize_port('http', '8080')
+    '8080'
     """
     if not scheme:
         return port
@@ -168,22 +201,28 @@ def normalize_port(scheme, port):
 
 
 def normalize_path(path):
-    """Normalize path (collapse etc.)
+    """Normalize path: collapse etc.
+
+    >>> normalize_path('/a/b///c')
+    '/a/b/c'
     """
-    if path in ['//', '/' ,'']:
+    if path in ['//', '/', '']:
         return '/'
-    npath = normpath(unquote(path, exceptions=UNQUOTE_EXCEPTIONS['path']))
+    npath = normpath(unquote(path, exceptions=QUOTE_EXCEPTIONS['path']))
     if path[-1] == '/' and npath != '/':
         npath += '/'
     return npath
 
 
 def normalize_query(query):
-    """Normalize query (sort params by name, remove params without value)
+    """Normalize query: sort params by name, remove params without value.
+
+    >>> normalize_query('z=3&y=&x=1')
+    'x=1&z=3'
     """
     if query == '' or len(query) <= 2:
         return ''
-    nquery = unquote(query, exceptions=UNQUOTE_EXCEPTIONS['query'])
+    nquery = unquote(query, exceptions=QUOTE_EXCEPTIONS['query'])
     params = nquery.split('&')
     nparams = []
     for param in params:
@@ -196,14 +235,27 @@ def normalize_query(query):
 
 
 def normalize_fragment(fragment):
-    """Normalize fragment (unquote with exceptions only)
-    """
-    return unquote(fragment, UNQUOTE_EXCEPTIONS['fragment'])
+    """Normalize fragment (unquote with exceptions only)."""
+    return unquote(fragment, QUOTE_EXCEPTIONS['fragment'])
+
+
+_hextochr = dict(('%02x' % i, chr(i)) for i in range(256))
+_hextochr.update(dict(('%02X' % i, chr(i)) for i in range(256)))
 
 
 def unquote(text, exceptions=[]):
-    """Unquote a text but ignore the exceptions
+    """Unquote a text but ignore the exceptions.
+
+    >>> unquote('foo%23bar')
+    'foo#bar'
+    >>> unquote('foo%23bar', ['#'])
+    'foo%23bar'
     """
+    if not text:
+        if text is None:
+            raise TypeError('None object cannot be unquoted')
+        else:
+            return text
     if '%' not in text:
         return text
     s = text.split('%')
@@ -221,20 +273,26 @@ def unquote(text, exceptions=[]):
 
 
 def parse(url):
-    """Parse a URL
+    """Parse a URL.
+
+    >>> parse('http://example.com/foo/')
+    URL(scheme='http', ..., domain='example', tld='com', ..., path='/foo/', ...)
     """
     parts = split(url)
     if parts.scheme:
-        (username, password, host, port) = split_netloc(parts.netloc)
-        (subdomain, domain, tld) = split_host(host)
+        username, password, host, port = split_netloc(parts.netloc)
+        subdomain, domain, tld = split_host(host)
     else:
         username = password = subdomain = domain = tld = port = ''
-    return ParseResult(parts.scheme, username, password, subdomain, domain, tld,
-                       port, parts.path, parts.query, parts.fragment)
+    return URL(parts.scheme, username, password, subdomain, domain, tld,
+               port, parts.path, parts.query, parts.fragment, url)
 
 
 def extract(url):
-    """Extract as much information from a (relative) URL as possible
+    """Extract as much information from a (relative) URL as possible.
+
+    >>> extract('example.com/abc')
+    URL(..., domain='example', tld='com', ..., path='/abc', ...)
     """
     parts = split(url)
     if parts.scheme:
@@ -244,17 +302,19 @@ def extract(url):
         netloc = parts.path
         path = ''
         if '/' in netloc:
-            tmp = netloc.split('/', 1)
-            netloc = tmp[0]
-            path = '/' + tmp[1]
-    (username, password, host, port) = split_netloc(netloc)
-    (subdomain, domain, tld) = split_host(host)
-    return ParseResult(parts.scheme, username, password, subdomain, domain, tld,
-                       port, path, parts.query, parts.fragment)
+            netloc, path_raw = netloc.split('/', 1)
+            path = '/' + path_raw
+    username, password, host, port = split_netloc(netloc)
+    subdomain, domain, tld = split_host(host)
+    return URL(parts.scheme, username, password, subdomain, domain, tld,
+               port, path, parts.query, parts.fragment, url)
 
 
 def split(url):
-    """Split URL into scheme, netloc, path, query and fragment
+    """Split URL into scheme, netloc, path, query and fragment.
+
+    >>> split('http://www.example.com/abc?x=1&y=2#foo')
+    SplitResult(scheme='http', netloc='www.example.com', path='/abc', query='x=1&y=2', fragment='foo')
     """
     scheme = netloc = path = query = fragment = ''
     ip6_start = url.find('[')
@@ -311,18 +371,22 @@ def split(url):
 
 
 def _clean_netloc(netloc):
-    """Remove trailing '.' and ':' and tolower
+    """Remove trailing '.' and ':' and tolower.
+
+    >>> _clean_netloc('eXample.coM:')
+    'example.com'
     """
     try:
-        netloc.encode('ascii')
+        return netloc.rstrip('.:').lower()
     except:
         return netloc.rstrip('.:').decode('utf-8').lower().encode('utf-8')
-    else:
-        return netloc.rstrip('.:').lower()
 
 
 def split_netloc(netloc):
-    """Split netloc into username, password, host and port
+    """Split netloc into username, password, host and port.
+
+    >>> split_netloc('foo:bar@www.example.com:8080')
+    ('foo', 'bar', 'www.example.com', '8080')
     """
     username = password = host = port = ''
     if '@' in netloc:
@@ -340,16 +404,22 @@ def split_netloc(netloc):
 
 
 def split_host(host):
-    """Use the Public Suffix List to split host into subdomain, domain and tld
+    """Use the Public Suffix List to split host into subdomain, domain and tld.
+
+    >>> split_host('foo.bar.co.uk')
+    ('foo', 'bar', 'co.uk')
     """
+    # host is IPv6?
     if '[' in host:
         return '', host, ''
-    domain = subdomain = tld = ''
+    # host is IPv4?
     for c in host:
         if c not in IP_CHARS:
             break
     else:
         return '', host, ''
+    # host is a domain name
+    domain = subdomain = tld = ''
     parts = host.split('.')
     for i in range(len(parts)):
         tld = '.'.join(parts[i:])
@@ -367,5 +437,5 @@ def split_host(host):
             tld = '.'.join(parts[i-1:])
             break
     if '.' in domain:
-        (subdomain, domain) = domain.rsplit('.', 1) 
+        subdomain, domain = domain.rsplit('.', 1)
     return subdomain, domain, tld
